605ba9cf389d5c8d6c80edc2aa997b0dde13fc30
Luca Baldini <luca.baldini@pi.infn.it>
2019-05-16 10:24:37 +0200

    Conference migration completed.

--- webpage/site.py
+++ webpage/site.py
@@ -274,171 +274,198 @@ CONFERENCE_LIST.add_conference(
 ).add_contribution(
     'The Fermi LAT Calorimeter as a gamma-ray telescope', poster=True
 )
-
-"""
 CONFERENCE_LIST.add_conference(
-    '',
-    '',
-    '',
-    '', ''
-).add_contribution(
-    ''
-)
-add('Cosmic rays and mark matter searches with Fermi',
     'Les Rencontres de Physique de la Vall&eacute;e d\'Aoste',
-    'La Thuile (Italy)', '27/02/2011', '05/03/2011',
-    'http://www.pi.infn.it/lathuile/lathuile_2011.html')
-
-
-add('The Fermi Large Area Telescope: highlights from the first two years in '
-    'orbit',
+    'La Thuile (Italy)',
+    'http://www.pi.infn.it/lathuile/lathuile_2011.html',
+    '2011-02-27', '2011-03-05'
+).add_contribution(
+    'Cosmic rays and mark matter searches with Fermi'
+)
+CONFERENCE_LIST.add_conference(
     'DICE 2010',
-    'Castiglioncello (Italy)', '13/09/2010', '17/09/2010',
+    'Castiglioncello (Italy)',
     'http://mail.df.unipi.it/~elze/DICE2010.html',
-    invited = True)
-
-
-add('Measurement of the cosmic-ray electron spectrum with the Fermi LAT',
-    '22nd European Cosmic-ray Symposium',
-    'Turku (Finland)', '03/08/2010', '06/08/2010',
-    'http://ecrs2010.utu.fi/',
-    invited = True)
-
-
-add('Fermi-LAT data monitoring',
+    '2010-09-13', '2010-09-17'
+).add_contribution(
+    'The Fermi Large Area Telescope: highlights from the first two years in orbit',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'Meeting Nazionale sull\'analisi dei dati dell\'osservatorio Fermi-LAT',
-    'Frascati', '01/10/2009', '02/10/2009',
-    'http://www.asdc.asi.it/fermimeeting/announcement.php')
-
-
-add('Experimental review of high-energy electron/positron and '
-    'proton/antiproton spectra',
+    'Frascati (Italy)',
+    'http://www.asdc.asi.it/fermimeeting/announcement.php',
+    '2010-10-01', '2010-10-01'
+).add_contribution(
+    'Fermi-LAT data monitoring'
+)
+CONFERENCE_LIST.add_conference(
+    '22nd European Cosmic-ray Symposium',
+    'Turku (Finland)',
+    'http://ecrs2010.utu.fi/',
+    '2010-08-03', '2010-08-06'
+).add_contribution(
+    'Measurement of the cosmic-ray electron spectrum with the Fermi LAT',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'TeV Particle Astrophysics 2009',
-    'SLAC (Menlo Park, USA)', '13/07/2009', '17/07/2009',
+    'SLAC (Menlo Park, USA)',
     'http://www-conf.slac.stanford.edu/tevpa09/',
-    invited = True)
-
-
-add('The Fermi LAT: highlights after one year in orbit and '
-    'measurement of the cosmic-ray electron spectrum',
+    '2009-07-13', '2009-07-17'
+).add_contribution(
+    'Experimental review of high-energy electron/positron and proton/antiproton spectra',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'Searching for the Origins of Cosmic Rays',
-    'Trondheim (Norway)', '15/06/2009', '18/06/2009',
-    'http://web.phys.ntnu.no/~mika/socor.html')
-
-
-add('Observation of the high-energy cosmic-ray electron spectrum with '
-    'Fermi and implications for dark matter scenarios',
+    'Trondheim (Norway)',
+    'http://web.phys.ntnu.no/~mika/socor.html',
+    '2009-06-15', '2009-06-18'
+).add_contribution(
+    'The Fermi LAT: highlights after one year in orbit and measurement of the cosmic-ray electron spectrum'
+)
+CONFERENCE_LIST.add_conference(
     'New Lights on Dark Matter',
-    'Perimeter Institute (Waterloo, Canada)', '11/06/2009', '13/06/2009',
-    'http://www.perimeterinstitute.ca/en/Events/New_Lights_on_Dark_Matter/'
-    'New_Lights_on_Dark_Matter/',
-    invited = True)
-
-
-add('The Fermi Large Area Telescope as a high-energy electron detector',
+    'Perimeter Institute (Waterloo, Canada)',
+    'http://www.perimeterinstitute.ca/en/Events/New_Lights_on_Dark_Matter/New_Lights_on_Dark_Matter/',
+    '2009-06-11', '2009-06-13'
+).add_contribution(
+    'Observation of the high-energy cosmic-ray electron spectrum with Fermi and implications for dark matter scenarios',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'RICAP 2009',
-    'Villa Mondragone (Monte Porzio Catone, Roma)', '13/05/2009', '15/05/2009',
+    'Villa Mondragone (Monte Porzio Catone, Roma)',
     'http://ricap09.roma2.infn.it/',
-    invited = True)
-
-
-add('The Fermi gamma-ray space telescope: design, construction, '
-    'launch and early science results',
+    '2009-05-13', '2009-05-15'
+).add_contribution(
+    'The Fermi Large Area Telescope as a high-energy electron detector',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'CERN Detector Seminar',
-    'CERN (Geneve, Switzerland)', '20/03/2009', None,
+    'CERN (Geneve, Switzerland)',
     'http://indico.cern.ch/conferenceDisplay.py?confId=54525',
-    invited = True)
-
-
-add('GRB observations with Fermi',
+    '2009-03-20'
+).add_contribution(
+    'The Fermi gamma-ray space telescope: design, construction, launch and early science results',
+    invited=True
+)
+CONFERENCE_LIST.add_conference(
     'Rencontres de Moriond on Very High-Energy Phenomena in the Universe',
-    'La Thuile (Italy)', '01/02/2009', '08/02/2009',
-    'http://moriond.in2p3.fr/J09/')
-
-
-add('The GLAST Large Area Telescope',
+    'La Thuile (Italy)',
+    'http://moriond.in2p3.fr/J09/',
+    '2009-02-01', '2009-02-08'
+).add_contribution(
+    'GRB observations with Fermi'
+)
+CONFERENCE_LIST.add_conference(
     '8th International Conference on Position Sensitive Detectors',
-    'Glasgow (Scotland)', '01/09/2008', '05/09/2008',
-    'http://www.psd8.physics.gla.ac.uk/index.php')
-
-
-add('LAT ground calibration: the test campaign',
+    'Glasgow (Scotland)',
+    'http://www.psd8.physics.gla.ac.uk/index.php',
+    '2008-09-01', '2008-09-05'
+).add_contribution(
+    'The GLAST Large Area Telescope'
+)
+CONFERENCE_LIST.add_conference(
     'Astrofisica Gamma dallo Spazio in Italia: AGILE e GLAST',
-    'Frascati (Roma, Italy)', '02/07/2007', '03/07/2007',
-    'http://old.inaf.it/news_cartella/Astrofisica-gamma-dallo-Spazio-in-Italia/'
-    '?noredirect=1')
-
-
-add('Pyhton nell\'esperimento Gamma-Ray Large Area Space Telescope',
+    'Frascati (Roma, Italy)',
+    'http://old.inaf.it/news_cartella/Astrofisica-gamma-dallo-Spazio-in-Italia/?noredirect=1',
+    '2007-07-02', '2007-07-03'
+).add_contribution(
+    'LAT ground calibration: the test campaign'
+)
+CONFERENCE_LIST.add_conference(
     'PyCon Uno',
-    'Firenze (Italy)', '09/06/2007', '10/06/2007',
-    'http://www.pycon.it/pycon1')
-
-
-add('The online monitor for the GLAST Calibration Unit beam test',
+    'Firenze (Italy)',
+    'http://www.pycon.it/pycon1',
+    '2007-06-09', '2007-06-10'
+).add_contribution(
+    'Pyhton nell\'esperimento Gamma-Ray Large Area Space Telescope'
+)
+CONFERENCE_LIST.add_conference(
     'SciNeGHE 2012',
-    'Portoferraio (Isola d\'Elba)', '20/06/2006', '22/06/2006',
+    'Portoferraio (Isola d\'Elba)',
     'http://glast.pi.infn.it/Elba06/Elba06.html',
-    poster = True)
-
-
-add('The Gamma-ray Large Area Space Telescope: an astro-particle '
-    'mission to explore the high-energy sky',
+    '2006-06-20', '2006-06-22'
+).add_contribution(
+    'The online monitor for the GLAST Calibration Unit beam test',
+    poster=True
+)
+CONFERENCE_LIST.add_conference(
     'IEEE Nuclear Science Symposium',
-    'San Juan (Puerto Rico)', '23/10/2005', '27/10/2005',
-    'http://www.nss-mic.org/2005/')
-
-
-add('Highlights and memorabilia from GLAST at INFN-Pisa',
+    'San Juan (Puerto Rico)',
+    'http://www.nss-mic.org/2005/',
+    '2005-10-23', '2005-10-27'
+).add_contribution(
+    'The Gamma-ray Large Area Space Telescope: an astro-particle mission to explore the high-energy sky'
+)
+CONFERENCE_LIST.add_conference(
     'GLAST LAT Tracker Construction Workshop',
-    'Castelfalfi (Firenze, Italy)', '23/09/2005', None,
-    'http://glast.pi.infn.it/celebration/celebration.html')
-
-
-add('Status of the GLAST tracker construction',
+    'Castelfalfi (Firenze, Italy)',
+    'http://glast.pi.infn.it/celebration/celebration.html',
+    '2005-09-23'
+).add_contribution(
+    'Highlights and memorabilia from GLAST at INFN-Pisa'
+)
+CONFERENCE_LIST.add_conference(
     '5th AGILE Science Workshop',
-    'Roma (Italy)', '02/02/2005', None,
-    'http://www.fisica.uniroma2.it/~tovastro/ann/050111-agile.html')
-
-
-add('The silicon-strip tracker of the Large Area Telescope',
+    'Roma (Italy)',
+    'http://www.fisica.uniroma2.it/~tovastro/ann/050111-agile.html',
+    '2005-02-02'
+).add_contribution(
+    'Status of the GLAST tracker construction'
+)
+CONFERENCE_LIST.add_conference(
     'IEEE Nuclear Science Symposium',
-    'Roma (Italy)', '16/10/2004', '22/10/2004',
-    'http://nss-mic-rtsd-2004.df.unipi.it/nsshome2004.html')
-
-
-add('The GLAST silicon tracker construction',
-    'RESMDD04',
-    'Firenze (Italy)', '10/10/2004', '13/10/2004',
-    'http://cern.ch/resmdd')
-
-
-add('The silicon tracker for the GLAST Mini-tower',
+    'Roma (Italy)',
+    'http://nss-mic-rtsd-2004.df.unipi.it/nsshome2004.html',
+    '2004-10-16', '2004-10-22'
+).add_contribution(
+    'The silicon-strip tracker of the Large Area Telescope'
+)
+CONFERENCE_LIST.add_conference(
+    'RESMDD 2004',
+    'Firenze (Italy)',
+    'http://cern.ch/resmdd',
+    '2004-10-10', '2004-10-13'
+).add_contribution(
+    'The GLAST silicon tracker construction'
+)
+CONFERENCE_LIST.add_conference(
     'Frontier Science: Physics and Astrophysics in Space',
-    'Villa Mondragone (Monte Porzio Catone, Italy)', '14/06/2004', '19/06/2004',
-    #'',
-    poster = True)
-
-
-add('A gas pixel detector for x-ray polarimetry',
+    'Villa Mondragone (Monte Porzio Catone, Italy)',
+    None,
+    '2004-06-14', '2004-06-19'
+).add_contribution(
+    'The silicon tracker for the GLAST Mini-tower', poster=True
+)
+CONFERENCE_LIST.add_conference(
     '9th Topical Seminar on Innovative Particle and Radiation Detectors',
-    'Siena (Italy)', '23/05/2004', '26/05/2004',
-    'http://www.bo.infn.it/sminiato/siena04.html')
-
-
-add('A novel gaseus x-Ray polarimeter: data analysis and simulation',
+    'Siena (Italy)',
+    'http://www.bo.infn.it/sminiato/siena04.html',
+    '2004-05-23', '2004-05-26'
+).add_contribution(
+    'A gas pixel detector for x-ray polarimetry'
+)
+CONFERENCE_LIST.add_conference(
     'Astronomical Telescopes and Instrumentation',
-    'Waikoloa (Hawaii, USA)', '22/08/2002', '28/08/2002',
-    'http://spie.org/x13667.xml')
-
-
-add('The silicon strip tracker of the Gamma-ray Large Area Space Telescope',
+    'Waikoloa (Hawaii, USA)',
+    'http://spie.org/x13667.xml',
+    '2002-08-22', '2002-08-28'
+).add_contribution(
+    'A novel gaseus x-Ray polarimeter: data analysis and simulation'
+)
+CONFERENCE_LIST.add_conference(
     '9th European Symposium on Semiconductor Detectors',
-    'Schloss Elmau (Germany)', '23/06/2002', '27/06/2002',
+    'Schloss Elmau (Germany)',
     'http://www.hll.mpg.de/elmau',
-    poster = True)
-
-"""
+    '2002-06-23', '2002-06-27'
+).add_contribution(
+    'The silicon strip tracker of the Gamma-ray Large Area Space Telescope',
+    poster=True
+)
 
 
 # Definition of the page menu.
