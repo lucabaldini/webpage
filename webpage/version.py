e66a46245798aa97507f877aab7857dc51b8ec26
Luca Baldini <luca.baldini@pi.infn.it>
2019-05-31 21:25:13 +0200

    "Prepare for tag 0.0.6."

--- webpage/version.py
+++ webpage/version.py
@@ -1,6 +1,6 @@
 # Automatically created by webpage/tag.py.
 # Do not edit by hand.
 
-version = "0.0.5"
-release_date = "2019-05-31 21:21:51.867174"
-git_revision = "97d439f13d8d3a54e3bcac8dc95c6e6e0a2289da"
+version = "0.0.6"
+release_date = "2019-05-31 21:25:13.400481"
+git_revision = "dcb9b0c2e97fad3cef27dcccc5e4ac0edd868075"
