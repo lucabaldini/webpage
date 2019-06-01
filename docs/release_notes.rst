107d42759635ff3504051887581235c909ddf7c3
Luca Baldini <luca.baldini@pi.infn.it>
2019-06-01 10:13:50 +0200

    "Prepare for tag 0.3.1."

--- docs/release_notes.rst
+++ docs/release_notes.rst
@@ -1,6 +1,8 @@
 Release notes
 =============
 
+
+*webpage 0.3.1 (3e6ec322ed9c93107d499cc47f270b7f15b09e53) - 2019-06-01 10:13:50.201774*
 * More tinkering with the release manager,
 
 
--- webpage/version.py
+++ webpage/version.py
@@ -1,6 +1,6 @@
 # Automatically created by webpage/tag.py, do not edit by hand.
 # pylint: skip-file
 #
-version = "0.3.0"
-release_date = "2019-06-01 10:11:07.878536"
-revision = "59cb646ed9876dda079d79f9b6714874ce86b888"
+version = "0.3.1"
+release_date = "2019-06-01 10:13:50.201774"
+revision = "3e6ec322ed9c93107d499cc47f270b7f15b09e53"
