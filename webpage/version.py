bd08fa5e907c71731c62a84e5aa629ac2f1b63df
Luca Baldini <luca.baldini@pi.infn.it>
2019-06-01 09:10:31 +0200

    "Prepare for tag 0.1.4."

--- webpage/version.py
+++ webpage/version.py
@@ -1,6 +1,6 @@
 # Automatically created by webpage/tag.py, do not edit by hand.
-    # pylint: skip-file
-    #
-    version = "0.1.3"
-    release_date = "2019-06-01 09:08:05.533799"
-    git_revision = "9a248b1a926bba31d06f1f5475c47923b6dae295"
+# pylint: skip-file
+#
+version = "0.1.4"
+release_date = "2019-06-01 09:10:31.690904"
+git_revision = "397e12164e00c0b6820564aab2576fc4b980f207"
