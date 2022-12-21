d5db55bd1685fa5e2cfd204bdd1c23291c18e993
Luca Baldini <luca.baldini@pi.infn.it>
2022-12-21 15:30:24 +0100

    "Prepare for tag 2.0.0."

--- webpage/version.py
+++ webpage/version.py
@@ -1,6 +1,6 @@
-# Automatically created by /home/data/work/webpage/webpage/tag.py, do not edit by hand.
+# Automatically created by /data/work/webpage/webpage/tag.py, do not edit by hand.
 # pylint: skip-file
 #
-version = "1.1.2"
-release_date = "2022-12-15 08:31:51.658643"
-revision = "f223d5d8d82bc62b48d5d11fc7f4b8a98e885512"
+version = "2.0.0"
+release_date = "2022-12-21 15:30:24.578148"
+revision = "66d03dc0639baa8a4cad2f35c9e4af1249d7cd63"
